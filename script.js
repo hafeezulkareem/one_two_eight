631bf7af2a772fc65b9d0a05cad4ed7f3cc7cede
hafeezulkareem <hafeezulkareem@gmail.com>
2021-09-23 15:22:32 +0530

    Add colors to tiles

--- script.js
+++ script.js
@@ -2,16 +2,6 @@ const qs = (selector) => document.querySelector(selector);
 
 const qsa = (selector) => document.querySelectorAll(selector);
 
-const tilesUIInformation = {
-   2: { bgColor: "#eee4da", color: "#776e65" },
-   4: { bgColor: "#eee1c9", color: "#776e65" },
-   8: { bgColor: "#f3b27a", color: "#ffffff" },
-   16: { bgColor: "#f69664", color: "#ffffff" },
-   32: { bgColor: "#f77c5f", color: "#ffffff" },
-   64: { bgColor: "#f75f3b", color: "#ffffff" },
-   128: { bgColor: "#edc53f", color: "#ffffff" },
-};
-
 const getRandomInt = (min, max) =>
    Math.floor(Math.random() * (max - min) + min);
 
@@ -53,15 +43,47 @@ const addInitialTiles = () => {
    board[secondTileRow][secondTileCol] = secondTile;
 };
 
+const addClassesToTile = (tile, number) => {
+   tile.classList.add("tile");
+   switch (number) {
+      case 2:
+         tile.classList.add("two");
+         break;
+      case 4:
+         tile.classList.add("four");
+         break;
+      case 8:
+         tile.classList.add("eight");
+         break;
+      case 16:
+         tile.classList.add("sixteen");
+         break;
+      case 32:
+         tile.classList.add("thirty-two");
+         break;
+      case 64:
+         tile.classList.add("sixty-four");
+         break;
+      case 128:
+         tile.classList.add("one-twenty-eight");
+         break;
+      case null:
+         break;
+      default:
+         tile.classList.add("extra");
+         break;
+   }
+};
+
 const updateUI = () => {
    const gameBoard = qs("#gameBoard");
 
    const { board } = state;
    board.forEach((row) => {
-      row.forEach((item) => {
+      row.forEach((number) => {
          const tile = document.createElement("div");
-         tile.innerHTML = item;
-         tile.className = "tile";
+         tile.innerHTML = number;
+         addClassesToTile(tile, number);
          gameBoard.appendChild(tile);
       });
    });
