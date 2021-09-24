24ddf8e62a45f14e0c2ea43a51983c9904fff248
hafeezulkareem <hafeezulkareem@gmail.com>
2021-09-24 15:50:01 +0530

    Fix issue when board is full

--- script.js
+++ script.js
@@ -263,7 +263,22 @@ const mergeBottom = (board) => {
    }
 };
 
+const isBoardFull = (board) => {
+   for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+         if (board[i][j] === null) {
+            return false;
+         }
+      }
+   }
+   return true;
+};
+
 const addANewTile = (board) => {
+   if (isBoardFull(board)) {
+      return;
+   }
+
    const tile = getRandomStartTile();
 
    let row = getRandomInt(0, 3);
